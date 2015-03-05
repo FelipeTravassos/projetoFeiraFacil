12efd565a339f9ae0e6c298ebf1ac7e221de909b
gilles.m.henriques <gilles.m.henriques@gmail.com>
2015-03-05 02:09:38 -0300

    inserindo a opcao de adicionar ao carrinho

--- Smartphones/Android/FeiraFacilSmartphone/src/com/esbr/feirafacilsmartphone/PromoActivity.java
+++ Smartphones/Android/FeiraFacilSmartphone/src/com/esbr/feirafacilsmartphone/PromoActivity.java
@@ -1,8 +1,6 @@
 package com.esbr.feirafacilsmartphone;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
 import org.json.JSONArray;
@@ -14,33 +12,18 @@ import com.esbr.feirafacilsmartphone.server.TaskAllProducts;
 import com.esbr.feirafacilsmartphone.supermercado.Produto;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.AlertDialog;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
-import android.text.Layout;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class PromoActivity extends Activity {
@@ -58,6 +41,12 @@ public class PromoActivity extends Activity {
 	 * The {@link ViewPager} that will host the section contents.
 	 */
 	ViewPager mViewPager;
+	
+	private AlertDialog alerta; 
+	
+	private ArrayList<Produto> values;
+	
+	private ArrayList<Produto> carrinho;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -65,13 +54,16 @@ public class PromoActivity extends Activity {
 		setContentView(R.layout.fragment_promo);
 
 	    getActionBar().setDisplayHomeAsUpEnabled(true);
-
 		
 		ListView lv = (ListView) findViewById(R.id.list_prod_promo);
+		
 		String result;
 		JSONArray json;
-		ArrayList<Produto> values = new ArrayList<Produto>();
+		values = new ArrayList<Produto>();
+		carrinho = new ArrayList<Produto>();
+		
 		Log.i("teste", "teste");
+		
 		try {
 			
 			result = new TaskAllProducts().execute().get();
@@ -95,6 +87,19 @@ public class PromoActivity extends Activity {
 			e.printStackTrace();
 		}        
 		
+		
+		lv.setOnItemClickListener(new OnItemClickListener() {
+            
+			@Override
+  			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				adicionarItemCarrinho(values.get(position));
+				
+			}
+        });
+		
+		
 		lv.setAdapter(new PromoArrayAdapter(this,values )); 
 
 				
@@ -108,12 +113,31 @@ public class PromoActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		
 		return super.onOptionsItemSelected(item);
 	}
+	
+	private void adicionarItemCarrinho(final Produto produto) { 
+		android.app.AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(produto.getNome()); 
+		builder.setMessage("Deseja adicionar este item ao carrinho?");
+		builder.setNegativeButton("Não", new DialogInterface.OnClickListener() { 
+			public void onClick(DialogInterface arg0, int arg1) { 
+				arg0.cancel();
+				} 
+			});
+		
+		builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() { 
+			public void onClick(DialogInterface arg0, int arg1) { 
+				carrinho.add(produto);
+				Toast.makeText(PromoActivity.this, "Item adicionado ao carrinho", Toast.LENGTH_SHORT).show();
+				} 
+			}); 
+		alerta = builder.create(); 
+		alerta.show();
+	}
 
-	
-	
+
+		
 	
 	
 
