e10c8d617ff438b5ce7c4fad448bba31aa6c1068
gilles.m.henriques <gilles.m.henriques@gmail.com>
2015-03-09 11:51:19 -0300

    correcao de bug - produto n era removido apos filtro

--- Smartphones/Android/FeiraFacilSmartphone/src/com/esbr/feirafacilsmartphone/adapter/CarrinhoArrayAdapter.java
+++ Smartphones/Android/FeiraFacilSmartphone/src/com/esbr/feirafacilsmartphone/adapter/CarrinhoArrayAdapter.java
@@ -125,6 +125,7 @@ public class CarrinhoArrayAdapter extends ArrayAdapter<Produto>{
 			public void onClick(DialogInterface arg0, int arg1) {
 				Carrinho.getInstance().removerItemCarrinho(produto);
 				Toast.makeText(context, "Concluído asda", Toast.LENGTH_SHORT).show();
+				remove(produto);
 				notifyDataSetChanged();
 				
 			} 
